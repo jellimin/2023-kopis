37ef3bad4aab4d1de4f1bd046ab13cd5947f2b8b
jellimin <pmy4983@naver.com>
2023-08-24 22:00:59 +0900

    1st commit

--- flask/website/auth.py
+++ flask/website/auth.py
@@ -38,7 +38,7 @@ def sign_up():
     if request.method == 'POST':
         # form - input의 name 속성을 기준으로 가져오기
         email = request.form.get('email')
-        nickname = request.form.get('nickname')
+        name = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
@@ -48,7 +48,7 @@ def sign_up():
             flash("이미 가입된 이메일입니다.", category='error')
         elif len(email) < 5 :
             flash("이메일은 5자 이상입니다.", category="error")
-        elif len(nickname) < 2:
+        elif len(name) < 2:
             flash("닉네임은 2자 이상입니다.", category="error")
         elif password1 != password2 :
             flash("비밀번호와 비밀번호재입력이 서로 다릅니다.", category="error")
@@ -56,7 +56,7 @@ def sign_up():
             flash("비밀번호가 너무 짧습니다.", category="error")
         else:
             # Create User > DB
-            new_user = User(email=email, nickname=nickname, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, nickname=name, password=generate_password_hash(password1, method='sha256'))
             # 생성한 User 인스턴스를 추가
             db.session.add(new_user)
             # commit하여 최종 반영
